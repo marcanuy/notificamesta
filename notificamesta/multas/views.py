d4b493b3798f0137f4257009d8c55d09b3ca4876
Marcelo Canina <me@marcanuy.com>
2017-05-21 16:11:16 -0300

    added multas chart

--- notificamesta/multas/views.py
+++ notificamesta/multas/views.py
@@ -4,6 +4,7 @@
 
 import click
 from notificamesta import db
+from notificamesta.multas.models import Contravencion
 from flask import Blueprint, session, flash, redirect, url_for, render_template
 #from .models import User
 
@@ -15,4 +16,18 @@ multas_blueprint = Blueprint(
 
 @multas_blueprint.route('/')
 def data():
-    return render_template('data.html')
+    sql="select * from (\
+    Select count(interseccion) as\
+    multas,interseccion from\
+    contravencion group by interseccion order by multas desc) \
+    as multaPorRadar order by multas desc limit 10"
+    l = db.engine.execute(sql).fetchall()
+    infraccionesPorRadar = [list(t) for t in zip(*l)]
+    #pairs up the elements from all inputs
+    print(infraccionesPorRadar)
+
+
+
+    #db.session.query(User).filter_by(id=current_user.id).update({"matricula": form.matricula.data})
+    #return render_template('data.html', top5)
+    return render_template('data.html', lista=infraccionesPorRadar)
