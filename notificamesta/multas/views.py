2060706b79bbbe40e35945d9de60b408debe186e
Marcelo Canina <me@marcanuy.com>
2017-05-23 11:56:40 -0300

    add chart by month in multas

--- notificamesta/multas/views.py
+++ notificamesta/multas/views.py
@@ -23,8 +23,8 @@ def data():
     contravencion group by interseccion order by multas desc) \
     as multaPorRadar order by multas desc limit 5"
     l = db.engine.execute(sql).fetchall()
-    infraccionesPorRadar = [list(t) for t in zip(*l)]
     #pairs up the elements from all inputs
+    infraccionesPorRadar = [list(t) for t in zip(*l)]
 
     sql_tipo_multa = "select count(*), valor, trim(articulo) from contravencion where valor!=0 group by valor, trim(articulo)"
     l = db.engine.execute(sql_tipo_multa).fetchall()
@@ -37,13 +37,31 @@ def data():
 
     sql_recaudado = "select sum(valor) from contravencion"
     recaudado_urs = db.engine.execute(sql_recaudado).fetchall()[0][0]
-    print(recaudado_urs)
-    print("---------------")
-    unidad_reajustable = 988
+
+    unidad_reajustable = 988 #Mayo 2017, @TODO poner UR por mes para mejorar calculo
     recaudado = recaudado_urs * unidad_reajustable
-    print("recaudad")
-    print(recaudado)
-    #db.session.query(User).filter_by(id=current_user.id).update({"matricula": form.matricula.data})
-    #return render_template('data.html', top5)
+
+    sql_multas_por_mes = "select to_char(mes, 'YYYY-mm'), multas, urs from \
+    (select date_trunc('month', fecha) as mes, count(*) as multas, sum(valor) as urs \
+    from contravencion \
+    group by mes \
+    order by mes) as multa"
+    l = db.engine.execute(sql_multas_por_mes).fetchall()
+    multas_por_mes = [list(t) for t in zip(*l)]
+
+    sql_fecha_actualizacion = "select to_char(fecha, 'YYYY-mm-dd') \
+    from contravencion \
+    order by fecha desc limit 1"
+    fecha_actualizacion = db.engine.execute(sql_fecha_actualizacion).fetchall()[0][0]
+
+
     maps_key=app.config['GOOGLE_MAPS_API_KEY']
-    return render_template('data.html', lista=infraccionesPorRadar, google_maps_api_key=maps_key, multas_por_radar=multas_por_radar, recaudado=recaudado, tipo_multa=tipo_multa, unidad_reajustable=unidad_reajustable)
+    return render_template('data.html', lista=infraccionesPorRadar,
+                           google_maps_api_key=maps_key,
+                           multas_por_radar=multas_por_radar,
+                           recaudado=recaudado,
+                           tipo_multa=tipo_multa,
+                           unidad_reajustable=unidad_reajustable,
+                           multas_por_mes=multas_por_mes,
+                           fecha_actualizacion=fecha_actualizacion
+    )
