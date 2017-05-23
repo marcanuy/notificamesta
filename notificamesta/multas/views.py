cfc17292e39d674b7aac3bec9a6513bda9de5830
Marcelo Canina <me@marcanuy.com>
2017-05-22 23:46:13 -0300

    fix chart with stats

--- notificamesta/multas/views.py
+++ notificamesta/multas/views.py
@@ -25,16 +25,25 @@ def data():
     l = db.engine.execute(sql).fetchall()
     infraccionesPorRadar = [list(t) for t in zip(*l)]
     #pairs up the elements from all inputs
-    print(infraccionesPorRadar)
 
-
+    sql_tipo_multa = "select count(*), valor, trim(articulo) from contravencion where valor!=0 group by valor, trim(articulo)"
+    l = db.engine.execute(sql_tipo_multa).fetchall()
+    tipo_multa = [list(t) for t in zip(*l)]
+    
     sql_multas_por_radar = "select * from (Select count(interseccion) as multas, r.latitud,r.longitud from contravencion as c \
     left join radar as r on r.id=c.radar_id \
     group by c.interseccion, r.id) as result where latitud!=''"
     multas_por_radar = db.engine.execute(sql_multas_por_radar).fetchall()
-    print(multas_por_radar)
-    
+
+    sql_recaudado = "select sum(valor) from contravencion"
+    recaudado_urs = db.engine.execute(sql_recaudado).fetchall()[0][0]
+    print(recaudado_urs)
+    print("---------------")
+    unidad_reajustable = 988
+    recaudado = recaudado_urs * unidad_reajustable
+    print("recaudad")
+    print(recaudado)
     #db.session.query(User).filter_by(id=current_user.id).update({"matricula": form.matricula.data})
     #return render_template('data.html', top5)
     maps_key=app.config['GOOGLE_MAPS_API_KEY']
-    return render_template('data.html', lista=infraccionesPorRadar, google_maps_api_key=maps_key, multas_por_radar=multas_por_radar)
+    return render_template('data.html', lista=infraccionesPorRadar, google_maps_api_key=maps_key, multas_por_radar=multas_por_radar, recaudado=recaudado, tipo_multa=tipo_multa, unidad_reajustable=unidad_reajustable)
